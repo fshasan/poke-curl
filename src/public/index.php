35e111b1ae4e3a20bcf6688c78791417286054df
Fsh-98 <sharukh75hasan@gmail.com>
2022-12-22 01:35:21 +0600

    added: style sheet and logo for pokedex.

--- src/public/index.php
+++ src/public/index.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <?php 
+        include "ref.php";
+    ?>
     <title>Pokedex</title>
 
 </head>
 <body>
-    <div class="container mt-5" style="float: center">
+    <div class="logo mt-5">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/269px-International_Pok%C3%A9mon_logo.svg.png" height="100%" alt="logo"/>
+    </div>
+    <div class="search-container mt-5">
         <form class="form-inline" method="GET">
             <div class="form-group mx-sm-3 mb-2">
                 <label class="mr-3">Pokemon</label>
@@ -23,18 +22,8 @@
         </form>
     </div>
 </body>
+
 </html>
-
-<script type="text/javascript">
-    function redirectToURL(btnId)
-    {
-        if(btnId=="resetBtn")
-        {
-            window.location.replace("http://localhost:8088/");
-        }
-
-    }
-</script>
 
 <?php 
 
@@ -58,3 +47,14 @@
     }
     
 ?>
+
+<script type="text/javascript">
+    function redirectToURL(btnId)
+    {
+        if(btnId=="resetBtn")
+        {
+            window.location.replace("http://localhost:8088/");
+        }
+
+    }
+</script>
