3563f7fd8de88c9e17500d5eeaf1b3a57819ed39
Fsh-98 <sharukh75hasan@gmail.com>
2023-01-16 11:36:11 +0600

    updated: title and autocomplete set to off.

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,7 @@
         include "ref.php";
         include "base.php";
     ?>
-    <title>Pokedex</title>
+    <title>Poke Curl</title>
 
 </head>
 <body>
@@ -17,7 +17,7 @@
         <div class="search-container mt-3">
             <form class="form-inline" method="GET">
                 <div class="form-group mx-sm-3 mb-2">
-                    <input type="text" class="form-control" id="searchID" name="search" required placeholder="Enter ID or Name">
+                    <input type="text" class="form-control" id="searchID" name="search" required placeholder="Enter ID or Name" autocomplete="off">
                 </div>
                 <button type="submit" id="search-id" class="btn btn-primary mb-2"><i class="bi bi-binoculars-fill"></i></button>
                 <button type="reset" id="resetBtn" class="btn btn-danger mb-2 ml-2" onClick='redirectToURL(this.id)'><i class="bi bi-stars"></i></button>
